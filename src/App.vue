fa00b3e7c3ea175d2bacbd293ba6a14297e1a631
SNiari <sihameniari@gmail.com>
2023-10-22 23:32:46 +0200

    Start

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <div class="container mx-auto">
+      <div class="flex items-center">
+        <img
+        class="logo"
+        src="@/assets/logo.svg"
+        width="125"
+        height="125"
+        />
+
+        <HelloWorld msg="You did it!" />
+      </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/app.css";
+</style>
